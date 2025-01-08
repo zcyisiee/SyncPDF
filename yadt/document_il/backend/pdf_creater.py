295071872da409b5bb6a1fdd8f75b7109f825420
awwaawwa <8493196+awwaawwa@users.noreply.github.com>
2025-01-08 15:05:40 +0800

    ✨ feat(pdf_creator): add dual PDF creation feature
    - implement functionality to generate dual PDFs with translation
    - support debug saving for decompressed output files

--- yadt/document_il/backend/pdf_creater.py
+++ yadt/document_il/backend/pdf_creater.py
@@ -37,13 +37,11 @@ class PDFCreater:
             return
         if graphic_state.stroking_color_space_name:
             draw_op.append(
-                f"/{graphic_state.stroking_color_space_name}"
-                f" CS \n".encode()
+                f"/{graphic_state.stroking_color_space_name}" f" CS \n".encode()
             )
         if graphic_state.non_stroking_color_space_name:
             draw_op.append(
-                f"/{graphic_state.non_stroking_color_space_name}"
-                f" cs \n".encode()
+                f"/{graphic_state.non_stroking_color_space_name}" f" cs \n".encode()
             )
         if graphic_state.ncolor is not None:
             draw_op.append(
@@ -130,9 +128,7 @@ class PDFCreater:
         pdf = pymupdf.open(self.original_pdf_path)
         self.add_font(pdf, self.docs)
         for page in self.docs.page:
-            encoding_length_map = {
-                f.font_id: f.encoding_length for f in page.pdf_font
-            }
+            encoding_length_map = {f.font_id: f.encoding_length for f in page.pdf_font}
             draw_op = BitStream()
             # q {ops_base}Q 1 0 0 1 {x0} {y0} cm {ops_new}
             draw_op.append(b"q ")
@@ -188,6 +184,17 @@ class PDFCreater:
             pdf[page.page_number].set_contents(op_container)
         pdf.save(mono_out_path, garbage=3, deflate=True)
         if translation_config.debug:
-            pdf.save(
-                f"{mono_out_path}.decompressed.pdf", expand=True, pretty=True
-            )
+            pdf.save(f"{mono_out_path}.decompressed.pdf", expand=True, pretty=True)
+
+        dual_out_path = translation_config.get_output_file_path(
+            f"{translation_config.input_file.rsplit('.', 1)[0]}."
+            f"{translation_config.lang_out}.dual.pdf"
+        )
+        dual = pymupdf.open(self.original_pdf_path)
+        dual.insert_file(pdf)
+        page_count = pdf.page_count
+        for id in range(page_count):
+            dual.move_page(page_count + id, id * 2 + 1)
+        dual.save(dual_out_path, garbage=3, deflate=True)
+        if translation_config.debug:
+            dual.save(f"{dual_out_path}.decompressed.pdf", expand=True, pretty=True)
