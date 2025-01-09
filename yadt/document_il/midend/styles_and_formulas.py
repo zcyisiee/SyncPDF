3f757ba4de87489956670ef1e5f21c692b83de2c
awwaawwa <8493196+awwaawwa@users.noreply.github.com>
2025-01-09 11:10:50 +0800

    ✨ feat(styles_and_formulas): add StylesAndFormulas class
    - implement initial structure for processing documents
    - define methods to process pages in documents

--- yadt/document_il/midend/styles_and_formulas.py
+++ yadt/document_il/midend/styles_and_formulas.py
@@ -1,6 +1,9 @@
-
+from yadt.document_il.il_version_1 import Page, Document
 
 class StylesAndFormulas:
-    def process(self, document):
+    def process(self, document:Document):
         for page in document.page:
             self.process_page(page)
+
+    def process_page(self, page: Page):
+        pass
