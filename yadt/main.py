a0fbe09beee7a0ae527713d0abd55aa4771b174f
awwaawwa <8493196+awwaawwa@users.noreply.github.com>
2025-01-24 20:25:02 +0800

    🔖 bump: release version 0.1.0.rc2
    - update version in pyproject.toml
    - update version in __init__.py
    - update version in main.py
    - increment release candidate version from rc1 to rc2

--- yadt/main.py
+++ yadt/main.py
@@ -14,7 +14,7 @@ from yadt.document_il.translator.translator import set_translate_rate_limiter
 from yadt.translation_config import TranslationConfig  # noqa: E402
 
 logger = logging.getLogger(__name__)
-__version__ = "0.1.0.rc1"
+__version__ = "0.1.0.rc2"
 
 def create_cache_folder():
     try:
