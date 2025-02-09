2d02c9ca8de4a59bacc6e266ae02f9b0b848d36b
awwaawwa <8493196+awwaawwa@users.noreply.github.com>
2025-02-09 16:08:22 +0800

    refactor(layout_parser): reduce batch size for page processing
    - decrease batch size from 48 to 16 for more frequent progress updates

--- yadt/document_il/midend/layout_parser.py
+++ yadt/document_il/midend/layout_parser.py
@@ -117,7 +117,7 @@ class LayoutParser:
         ) as progress:
 
             # Process pages in batches
-            batch_size = 48
+            batch_size = 16
             for i in range(0, total, batch_size):
                 self.translation_config.raise_if_cancelled()
                 batch_pages = pages_to_translate[i : i + batch_size]
