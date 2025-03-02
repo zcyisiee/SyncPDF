50df6bb02cfe3392e68ded48284c98cf11973f28
awwaawwa <8493196+awwaawwa@users.noreply.github.com>
2025-03-03 03:29:32 +0800

    chore: Bump version to 0.1.20

--- babeldoc/main.py
+++ babeldoc/main.py
@@ -23,7 +23,7 @@ from babeldoc.docvision.rpc_doclayout import RpcDocLayoutModel
 from babeldoc.translation_config import TranslationConfig
 
 logger = logging.getLogger(__name__)
-__version__ = "0.1.19"
+__version__ = "0.1.20"
 
 
 def create_parser():
