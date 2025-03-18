5f86f96a90e01927d5582d7ca00ec84aa9db760e
awwaawwa <8493196+awwaawwa@users.noreply.github.com>
2025-03-18 18:44:24 +0800

    chore(version): bump version to 0.2.9 in pyproject.toml and related files
    - Updated version number from 0.2.8 to 0.2.9 in `pyproject.toml`, `babeldoc/__init__.py`, `babeldoc/const.py`, and `babeldoc/main.py`.

--- babeldoc/main.py
+++ babeldoc/main.py
@@ -22,7 +22,7 @@ from babeldoc.translation_config import TranslationConfig
 from babeldoc.translation_config import WatermarkOutputMode
 
 logger = logging.getLogger(__name__)
-__version__ = "0.2.8"
+__version__ = "0.2.9"
 
 
 def create_parser():
