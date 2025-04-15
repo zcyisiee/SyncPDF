d05b00b6a8a83ffecd06fc0e92abf6d6b9de01c1
awwaawwa <8493196+awwaawwa@users.noreply.github.com>
2025-04-15 22:35:52 +0800

    fix(il_creater): enhance font parsing by adding WinAnsiEncoding support and handling encoding differences

--- babeldoc/document_il/frontend/il_creater.py
+++ babeldoc/document_il/frontend/il_creater.py
@@ -78,7 +78,267 @@ def get_name_cbox(face, name):
     return get_glyph_cbox(face, g)
 
 
-def parse_font_file(doc, idx, encoding):
+WinAnsiEncoding = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31,
+    32,
+    33,
+    34,
+    35,
+    36,
+    37,
+    38,
+    39,
+    40,
+    41,
+    42,
+    43,
+    44,
+    45,
+    46,
+    47,
+    48,
+    49,
+    50,
+    51,
+    52,
+    53,
+    54,
+    55,
+    56,
+    57,
+    58,
+    59,
+    60,
+    61,
+    62,
+    63,
+    64,
+    65,
+    66,
+    67,
+    68,
+    69,
+    70,
+    71,
+    72,
+    73,
+    74,
+    75,
+    76,
+    77,
+    78,
+    79,
+    80,
+    81,
+    82,
+    83,
+    84,
+    85,
+    86,
+    87,
+    88,
+    89,
+    90,
+    91,
+    92,
+    93,
+    94,
+    95,
+    96,
+    97,
+    98,
+    99,
+    100,
+    101,
+    102,
+    103,
+    104,
+    105,
+    106,
+    107,
+    108,
+    109,
+    110,
+    111,
+    112,
+    113,
+    114,
+    115,
+    116,
+    117,
+    118,
+    119,
+    120,
+    121,
+    122,
+    123,
+    124,
+    125,
+    126,
+    127,
+    8364,
+    0,
+    8218,
+    402,
+    8222,
+    8230,
+    8224,
+    8225,
+    710,
+    8240,
+    352,
+    8249,
+    338,
+    0,
+    381,
+    0,
+    0,
+    8216,
+    8217,
+    8220,
+    8221,
+    8226,
+    8211,
+    8212,
+    732,
+    8482,
+    353,
+    8250,
+    339,
+    0,
+    382,
+    376,
+    160,
+    161,
+    162,
+    163,
+    164,
+    165,
+    166,
+    167,
+    168,
+    169,
+    170,
+    171,
+    172,
+    173,
+    174,
+    175,
+    176,
+    177,
+    178,
+    179,
+    180,
+    181,
+    182,
+    183,
+    184,
+    185,
+    186,
+    187,
+    188,
+    189,
+    190,
+    191,
+    192,
+    193,
+    194,
+    195,
+    196,
+    197,
+    198,
+    199,
+    200,
+    201,
+    202,
+    203,
+    204,
+    205,
+    206,
+    207,
+    208,
+    209,
+    210,
+    211,
+    212,
+    213,
+    214,
+    215,
+    216,
+    217,
+    218,
+    219,
+    220,
+    221,
+    222,
+    223,
+    224,
+    225,
+    226,
+    227,
+    228,
+    229,
+    230,
+    231,
+    232,
+    233,
+    234,
+    235,
+    236,
+    237,
+    238,
+    239,
+    240,
+    241,
+    242,
+    243,
+    244,
+    245,
+    246,
+    247,
+    248,
+    249,
+    250,
+    251,
+    252,
+    253,
+    254,
+    255,
+]
+
+
+def parse_font_file(doc, idx, encoding, differences):
     bbox_list = []
     data = doc.xref_stream(idx)
     face = freetype.Face(BytesIO(data))
@@ -86,9 +346,9 @@ def parse_font_file(doc, idx, encoding):
         if charmap.encoding_name == "FT_ENCODING_ADOBE_CUSTOM":
             face.select_charmap(freetype.FT_ENCODING_ADOBE_CUSTOM)
             break
-    bbox_list = [get_char_cbox(face, x) for x in range(0, 256)]
-    if encoding:
-        for code, name in encoding:
+    bbox_list = [get_char_cbox(face, x) for x in encoding]
+    if differences:
+        for code, name in differences:
             bbox_list[code] = get_name_cbox(face, name.encode("U8"))
     return bbox_list
 
@@ -112,18 +372,23 @@ def parse_encoding(obj_str):
 def parse_mapping(text):
     mapping = []
     for x in re.finditer(r"<(?P<num>[a-fA-F0-9]+)>", text):
-        mapping.append(int(x.group("num"), 16))
+        mapping.append(x.group("num"))
     return mapping
 
 
 def update_cmap_pair(cmap, data):
-    for start, stop, value in batched(data, 3):
+    for start_str, stop_str, value_str in batched(data, 3):
+        start = int(start_str, 16)
+        stop = int(stop_str, 16)
+        value = base64.b16decode(value_str).decode("UTF-16-BE")
         for code in range(start, stop + 1):
             cmap[code] = value
 
 
 def update_cmap_code(cmap, data):
-    for code, value in batched(data, 2):
+    for code_str, value_str in batched(data, 2):
+        code = int(code_str, 16)
+        value = base64.b16decode(value_str).decode("UTF-16-BE")
         cmap[code] = value
 
 
@@ -456,14 +721,19 @@ class ILCreater:
             self.current_page.pdf_font.append(il_font_metadata)
 
     def parse_font_xobj_id(self, xobj_id: int):
-        encoding = []
-        font_encoding = self.mupdf.xref_get_key(xobj_id, "Encoding/Differences")
-        if font_encoding:
-            encoding = parse_encoding(font_encoding[1])
         bbox_list = []
-        font_file = self.mupdf.xref_get_key(xobj_id, "FontDescriptor/FontFile")
-        if file_idx := indirect(font_file):
-            bbox_list = parse_font_file(self.mupdf, file_idx, encoding)
+        encoding = list(range(256))
+        font_encoding = self.mupdf.xref_get_key(xobj_id, "Encoding")
+        if font_encoding[1] == "/WinAnsiEncoding":
+            encoding = WinAnsiEncoding
+        differences = []
+        font_differences = self.mupdf.xref_get_key(xobj_id, "Encoding/Differences")
+        if font_differences:
+            differences = parse_encoding(font_differences[1])
+        for file_key in ["FontFile", "FontFile2", "FontFile3"]:
+            font_file = self.mupdf.xref_get_key(xobj_id, f"FontDescriptor/{file_key}")
+            if file_idx := indirect(font_file):
+                bbox_list = parse_font_file(self.mupdf, file_idx, encoding, differences)
         cmap = {}
         to_unicode = self.mupdf.xref_get_key(xobj_id, "ToUnicode")
         if to_unicode_idx := indirect(to_unicode):
