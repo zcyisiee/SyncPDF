2c499ea2c33bd78953aebd33160bf55ca50205d8
awwaawwa <8493196+awwaawwa@users.noreply.github.com>
2025-04-30 11:27:39 +0800

    fix: bump version to 0.3.29

--- babeldoc/main.py
+++ babeldoc/main.py
@@ -23,7 +23,7 @@ from babeldoc.translation_config import TranslationConfig
 from babeldoc.translation_config import WatermarkOutputMode
 
 logger = logging.getLogger(__name__)
-__version__ = "0.3.28"
+__version__ = "0.3.29"
 
 
 def create_parser():
