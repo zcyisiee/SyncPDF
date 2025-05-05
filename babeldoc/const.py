66695dc344ae476ba4bea3585324331b6838ae5e
awwaawwa <8493196+awwaawwa@users.noreply.github.com>
2025-05-06 00:00:34 +0800

    chore: bump version to 0.3.34

--- babeldoc/const.py
+++ babeldoc/const.py
@@ -3,7 +3,7 @@ import shutil
 import subprocess
 from pathlib import Path
 
-__version__ = "0.3.33"
+__version__ = "0.3.34"
 
 CACHE_FOLDER = Path.home() / ".cache" / "babeldoc"
 
--- babeldoc/main.py
+++ babeldoc/main.py
@@ -23,7 +23,7 @@ from babeldoc.translation_config import TranslationConfig
 from babeldoc.translation_config import WatermarkOutputMode
 
 logger = logging.getLogger(__name__)
-__version__ = "0.3.33"
+__version__ = "0.3.34"
 
 
 def create_parser():
