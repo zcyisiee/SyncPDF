ce816e10fbf6a27d9a5085396191f05fc5ccb213
lastsummerx <lastsummerx@163.com>
2025-05-18 01:01:18 +0800

    change zstd to pyzstd

--- babeldoc/document_il/utils/zstd_helper.py
+++ babeldoc/document_il/utils/zstd_helper.py
@@ -1,6 +1,6 @@
 import base64
 
-import zstd
+import pyzstd
 
 
 def zstd_compress(data) -> str:
@@ -9,7 +9,7 @@ def zstd_compress(data) -> str:
     if not isinstance(data, bytes):
         raise TypeError(f"data must be str or bytes, not {type(data)}")
 
-    return base64.b85encode(zstd.compress(data)).decode()
+    return base64.b85encode(pyzstd.compress(data)).decode()
 
 
 def zstd_decompress(data) -> str:
@@ -18,4 +18,4 @@ def zstd_decompress(data) -> str:
     if not isinstance(data, bytes):
         raise TypeError(f"data must be str or bytes, not {type(data)}")
 
-    return zstd.decompress(base64.b85decode(data)).decode()
+    return pyzstd.decompress(base64.b85decode(data)).decode()
