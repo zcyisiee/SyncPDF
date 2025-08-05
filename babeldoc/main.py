c8b35ee74710150b5b493faf77639cb664871d56
awwaawwa <8493196+awwaawwa@users.noreply.github.com>
2025-08-05 14:39:30 +0800

    chore(version): bump version to 0.4.21

--- babeldoc/main.py
+++ babeldoc/main.py
@@ -22,7 +22,7 @@ from babeldoc.translator.translator import OpenAITranslator
 from babeldoc.translator.translator import set_translate_rate_limiter
 
 logger = logging.getLogger(__name__)
-__version__ = "0.4.20"
+__version__ = "0.4.21"
 
 
 def create_parser():
